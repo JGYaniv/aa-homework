8a6072c90d2696a608656a514f7265e091dd66ec
Jonathan <yoniyaniv@gmail.com>
2020-04-15 22:31:46 -0400

    updated rails demo w05d04

--- W05D04/demo_project/app/models/person.rb
+++ W05D04/demo_project/app/models/person.rb
@@ -1,3 +1,11 @@
+# == Schema Information
+#
+# Table name: people
+#
+#  id       :bigint           not null, primary key
+#  name     :string           not null
+#  house_id :integer
+#
 class Person < ApplicationRecord
     validates :name, presence: true
 
