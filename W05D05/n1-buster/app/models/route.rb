9d564069e925b2c1ddfe7cdd85ec58f2c7a22dd9
Jonathan <yoniyaniv@gmail.com>
2020-04-17 00:17:11 -0400

    completed w5d5 hw assignments

--- W05D05/n1-buster/app/models/route.rb
+++ W05D05/n1-buster/app/models/route.rb
@@ -5,7 +5,7 @@ class Route < ApplicationRecord
     primary_key: :id
 
   def n_plus_one_drivers
-    buses = self.buses
+    buses = self.buses.includes(:drivers)
 
     all_drivers = {}
     buses.each do |bus|
@@ -20,6 +20,17 @@ class Route < ApplicationRecord
   end
 
   def better_drivers_query
-    # TODO: your code here
+    buses = self.buses.includes(:drivers)
+
+    all_drivers = {}
+    buses.each do |bus|
+      drivers = []
+      bus.drivers.each do |driver|
+        drivers << driver.name
+      end
+      all_drivers[bus.id] = drivers
+    end
+
+    all_drivers
   end
 end
